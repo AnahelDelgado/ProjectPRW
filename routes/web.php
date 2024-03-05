33f4f83b39429bdc1f44e92e019b9c59ba5a9a07
oscarlmj <oscarlmorenojorge@gmail.com>
2024-03-05 18:50:44 +0000

    Se crean las rutas para el login con la API de Google

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
+use App\Models\teacher;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,29 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/login', 'App\Http\Controllers\menuController@showLogin')->name('secciones.login');
+
+Route::get('/auth/google', function () {
+    return Socialite::driver('google')->redirect();
+});
+
+Route::get('/auth/google/callback', function () {
+    $user = Socialite::driver('google')->stateless()->user();
+
+    $userExist = teacher::where('email', $user->email)->first();
+
+    if($userExist){
+        Auth::login($userExist);
+    }else {
+        $userNew = teacher::create([
+            'nombre' => $user->name,
+            'email' => $user->email,
+            'avatar' => $user->avatar,
+            'external_id' => (string) $user->id,
+            'external_auth' => 'google',
+        ]);
+        Auth::login($userNew);
+    }
+
+    return redirect('/login');
+});
+
