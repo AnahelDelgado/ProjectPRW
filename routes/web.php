3f23fc656a224c8c6e8bf6098a2027b3fcf5507e
Judith soro Corvo <judithsorocorvo@alumno.ieselrincon.es>
2024-03-06 09:08:55 +0000

    actualizacion layout

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/login', 'App\Http\Controllers\menuController@showLogin')->name('secciones.login');
 Route::get('/ReservaAula', 'App\Http\Controllers\reservasController@reserva1')->name('secciones.layout');
-Route::get('/ReservaMaterial', 'App\Http\Controllers\reservasController@reserva1')->name('secciones.layout');
+Route::get('/ReservaMaterial', 'App\Http\Controllers\reservasController@reserva2')->name('secciones.layout');
