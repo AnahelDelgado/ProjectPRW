5dd217e5d5a5ef7757d4394b626c4a724be6cdd1
Victor-Plagaro <plagaro26@gmail.com>
2024-03-11 15:51:47 +0000

    Cambios para poder hacer un pull

--- resources/views/secciones/login.blade.php
+++ resources/views/secciones/login.blade.php
@@ -1,55 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{asset('/css/index.css')}}">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="{{asset('/css/index.css')}}">
+  <!-- Google Font -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <title>Login Profesorado</title>
 </head>
-<body class="bodyLogin">
-
-
-<div class="titulo">
-
-<h2>Reserva tu aula</h2>
-<p class="tp">Inicia sesión aquí para poder reservar </p>
-
-</div>
-
-
-
-
-<div class="CuadradoBlanco">
-
-<form class="formulario" action="" method="get">
-
-  <h1 class="sesion">INICIAR SESIÓN</h1>
-  <div class="f2">
-  <p>Correo: <input type="email" name="email" maxlength="6" placeholder="nombre@profesor.ieselri.."></p>
-  <p>Contraseña: <input type="password" name="password" maxlength="6" placeholder="************"></p>
-  <p>
-    <input type="submit" value="Aceptar" class="boton">
-  </p>
-  </div>
-</form>
-
- <!-- Botón de Iniciar sesión con Google -->
- <div class="google-login">
-        <a href="/auth/google" class="google-btn">
-            <img src="https://developers.google.com/identity/images/g-logo.png" alt="Google Logo" class="google-icon"> Iniciar sesión con Google
-        </a>
+<body>
+  <section class="container">
+    <div class="content">
+      <img src="/img/a_de_ateca.png" alt="Foto del aula" class="logoAteca">
     </div>
 
-</div>
-    
-<footer>
+    <div class="content">
+      <div class="wrapper-form">
 
-<img class="logo" src="/img/a_de_ateca.png" />
+        <h3>Inicio de Sesión</h3>
+        <form action="">
+          <div class="wrapper-input">
+            <label for="emailUser">Correo electrónico</label>
+          </div>
+            <input type="email" name="emailUser" id="emailUser" placeholder="Correo electronico">
 
-</footer>
-<div class="link">
-<a  href="https://www3.gobiernodecanarias.org/medusa/edublog/ieselrincon/">I.E.S El Rincón</a>.
-</div>
+          <div class="wrapper-input">
+            <label for="psswdUser">Contraseña</label>
+          </div>
+          <input type="password" name="psswdUser" id="psswdUser" placeholder="Contrasenia">
 
+          <input type="submit" value="Login">
+        </form>
+
+      </div>
+    </div>
+  </section>
 </body>
 </html>
