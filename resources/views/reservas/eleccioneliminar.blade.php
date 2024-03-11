bbe7f57b028c3fd944b91025536289ea4b73e8e8
Anahel Delgado <anahelginesdelgadoalmeida@alumno.ieselrincon.es>
2024-03-11 11:50:00 +0000

    Eliminación del logo en las elecciones

--- resources/views/reservas/eleccioneliminar.blade.php
+++ resources/views/reservas/eleccioneliminar.blade.php
@@ -1,19 +1,12 @@
 @extends('layout.layout2')
 @section('head')
-<link rel="stylesheet" href="{{asset('/css/eleccion2.css')}}">
+<link rel="stylesheet" href="{{asset('/css/eleccion.css')}}">
 @endsection
 @section('content')
-        <?php
-        if (session()->get('user') === null) {
-            header("Location: /login");
-            exit;
-        }
-        ?>
 
 <div class="buttons">
     <a href="/reservas/eliminar" class="button">Eliminar reserva aula</a>
     <a href="/reservas/eliminarMaterial" class="button">Eliminar reserva material</a>
-   
 </div>
 
 @endsection
