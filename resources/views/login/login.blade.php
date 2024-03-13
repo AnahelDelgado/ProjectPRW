8abc61ada547aeb74a52fe72bfaee9639f4a9cfd
Judith soro Corvo <judithsorocorvo@alumno.ieselrincon.es>
2024-03-13 11:55:00 +0000

    login

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -1,35 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="{{asset('/css/login.css')}}">
   <title>Inicio sesion</title>
 </head>
-<body class="bodyLogin">
-  <div class="titulo">
-    <h2>Reserva tu aula y material</h2>
-    <p class="tp">Inicia sesión aquí para poder reservar </p>
-  </div>
-  <div class="CuadradoBlanco">
-    <form class="formulario" action="" method="get">
-      <h1 class="sesion">INICIAR SESIÓN CON GOOGLE</h1>
-     
-    </form>
 
-    <!-- Botón de Iniciar sesión con Google -->
-    <div class="google-login">
-      <a href="/auth/google" class="google-btn">
-        <img src="https://developers.google.com/identity/images/g-logo.png" alt="Google Logo" class="google-icon">
-      </a>
-      <p class="textoGoogle"> Iniciar sesión con google</p>
+<body class="bodyLogin">
+  <div class="cont">
+    <h2 class="titulo">Reserva tu aula y material</h2>
+    <p class="titulo">Inicia sesión aquí para poder reservar </p>
+
+    <div class="CuadradoBlanco">
+      <form class="formulario" action="" method="get">
+        <h1 class="sesion">INICIAR SESIÓN CON GOOGLE</h1>
+
+      </form>
+
+      <!-- Botón de Iniciar sesión con Google -->
+      <div class="google-login">
+        <a href="/auth/google" class="google-btn">
+          <img src="https://developers.google.com/identity/images/g-logo.png" alt="Google Logo" class="google-icon">
+        </a>
+        <p class="textoGoogle"> Iniciar sesión con google</p>
+
+      </div>
     </div>
-  </div>
-  <footer>
-    <img class="logo" src="/img/a_de_ateca.png" />
-  </footer>
-  <div class="link">
-    <a href="https://www3.gobiernodecanarias.org/medusa/edublog/ieselrincon/">I.E.S El Rincón</a>.
+    <footer>
+      <img class="logo" src="/img/a_de_ateca.png" />
+    </footer>
   </div>
 </body>
+
 </html>
